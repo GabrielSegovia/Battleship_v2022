7e69b92be38d255c3bbe0bf4e01f45609de1fa09
GabrielSegovia <88328974+GabrielSegovia@users.noreply.github.com>
2022-01-27 22:28:14 -0500

    Update apropos.js

--- src/pages/apropos.js
+++ src/pages/apropos.js
@@ -12,23 +12,19 @@ function Apropos() {
         <div className='textBox'>
         <h1>Gabriel Segovia-Fuentes</h1>
         <h2>À propos du dévellopeur</h2>
-        <p>Programmeur depuis 2021 mais createur de site web avec d'autres plateformes depuis 2017,
-          
+        <p>(Programmeur depuis 2021 mais createur de site web avec d'autres plateformes depuis 2017,
           
           la réalisation de projet à 
           toujours été une passion et une source de motivation. Ces projets d'équipes ont gagnés des prix tels que le celui
           pour l'entreprenariat avec le college Brébeuf et un trophé dans le Latin Awards Canada avec le podcast de Musique Sans Frontière.
           Comme expérience en travail il y a aussi une quinzaine d'année d'expérience de service à la clientèle ainsi que le service avec les fournisseurs tels que Nike, 
-          Adidas et Puma en autres avec la boutique familliale acquise depuis 2007 spécialisé dans le soccer. Dans l'immédiat, nous avons plusieurs école dont nous 
+          Adidas et Puma en autres avec la boutique familliale acquise depuis 2007 spécialisé dans le soccer. Dans l'immédiat, nous avons plusieurs écoles dont nous 
           leur fournissons la production de vêtements ainsi que la réalisation du site web de leur boutique, car c'est le future pour nous tous.
-          
-          Maintenant avec la spécialisation de dévelleppement de site web, je souhaite appliquer mes connaissances
-          dans le monde de l'internet et de la programmation en apportant une petite touche d'inspiration dans les travail de groupe et la communauté des int
-          
-          est devenue un nouveau mode de vie.
-          avant de commencer les études en programmation de site web transactionel, les projets étaient déjà du quotient,  </p>
-        
-        <p>Lien github</p>
+          Maintenant avec la spécialisation dans le dévelleppement de site web, viens le souhait d'appliquer les connaissances aquices
+          dans le monde de l'internet et de la programmation en apportant une petite touche d'inspiration dans les travaux de groupe et la communauté des internautes.
+          </p>
+
+        <a href="https://github.com/GabrielSegovia/Battleship_v2022.git"> GitHub BattleShip </a>
       </div>
       </div>
       <br></br>
