e9590f41c822171d9a89fda27b06bb787e7643b7
GabrielSegovia <88328974+GabrielSegovia@users.noreply.github.com>
2022-01-27 20:32:34 -0500

    Update apropos.js

--- src/pages/apropos.js
+++ src/pages/apropos.js
@@ -12,7 +12,22 @@ function Apropos() {
         <div className='textBox'>
         <h1>Gabriel Segovia-Fuentes</h1>
         <h2>À propos du dévellopeur</h2>
-        <p>Programmeur depuis 2021 avec 15 d'expérience de service à la clientèle ainsi que le service avec les fournisseurs tels que Nike, Adidas et Puma en autres. Maintenant submergé dans le monde de l'informatique, la programmation et le travail de groupe est devenue un nouveau mode de vie. </p>
+        <p>Programmeur depuis 2021 mais createur de site web avec d'autres plateformes depuis 2017,
+          
+          
+          la réalisation de projet à 
+          toujours été une passion et une source de motivation. Ces projets d'équipes ont gagnés des prix tels que le celui
+          pour l'entreprenariat avec le college Brébeuf et un trophé dans le Latin Awards Canada avec le podcast de Musique Sans Frontière.
+          Comme expérience en travail il y a aussi une quinzaine d'année d'expérience de service à la clientèle ainsi que le service avec les fournisseurs tels que Nike, 
+          Adidas et Puma en autres avec la boutique familliale acquise depuis 2007 spécialisé dans le soccer. Dans l'immédiat, nous avons plusieurs école dont nous 
+          leur fournissons la production de vêtements ainsi que la réalisation du site web de leur boutique, car c'est le future pour nous tous.
+          
+          Maintenant avec la spécialisation de dévelleppement de site web, je souhaite appliquer mes connaissances
+          dans le monde de l'internet et de la programmation en apportant une petite touche d'inspiration dans les travail de groupe et la communauté des int
+          
+          est devenue un nouveau mode de vie.
+          avant de commencer les études en programmation de site web transactionel, les projets étaient déjà du quotient,  </p>
+        
         <p>Lien github</p>
       </div>
       </div>
